32f6dea596601843c05c04ea4e74326a1f7ee8f8
DuplicateP <modassirjawaid44@gmail.com>
2026-03-18 13:26:16 +0530

    Refactor vars.py to update environment variable defaults

--- modules/vars.py
+++ modules/vars.py
@@ -7,14 +7,14 @@ API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH"," ")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", ""))
+OWNER = int(environ.get("OWNER", "7357284659"))
 CREDIT = environ.get("CREDIT", '<a href="https://t.me/Jetha_lal_bot"𝗠𝗢𝗗𝗔𝗦𝗦𝗜𝗥 ⚡  💻</a>')
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7357284659').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7357284659').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
